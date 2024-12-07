1a10856f683df5b81534a8c88d199bb3dfaf58ee
Joao Paulo <joaopaulo.inf@gmail.com>
2024-12-07 08:44:18 -0300

    Update AWSS3Sync.ps1

--- Scripts/AWSS3Sync.ps1
+++ Scripts/AWSS3Sync.ps1
@@ -1,7 +1,7 @@
-$S3Bucket = "s3://my-bucket/path"
+$S3Bucket = "s3://my-bucket/path/"
 $localPath = "/local/path/"
 
 $hostName = get-content env:computername
-$Data = (Get-Date (Get-Date).addDays(-2) -UFormat "%Y-%m-%d")
-aws s3 sync $S3Bucket $localPath --query 'Contents[LastModifiedd=$Data][].key'
-aws s3 sync $localPath $S3Bucket --query 'Contents[LastModifiedd=$Data][].key'
+
+aws s3 sync $S3Bucket$hostName $localPath
+aws s3 sync $localPath $S3Bucket$hostName
